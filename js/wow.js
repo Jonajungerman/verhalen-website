83f0f85ba6c74c9db797e46508f13cb25090941a
Jonajungerman <45544679+Jonajungerman@users.noreply.github.com>
2019-01-14 23:23:24 +0100

    Add files via upload

---
+++ js/wow.js
@@ -0,0 +1,25 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+// zet de items in een array
+
+
+/*
+for (i = 0; i < sterArray.length; i++) {
+    sterArray[i].classList.add('before');
+}
+
+function veranderClass(){
+    this.classList.toggle('before');
+    this.previousElementSibling.classList.toggle('after');
+}
+
+for (i = 0; i < sterArray.length; i++) {
+    sterArray[i].addEventListener('click', veranderClass);
+}
+
+*/
+
+
+
